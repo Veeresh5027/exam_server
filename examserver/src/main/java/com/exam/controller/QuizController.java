92a1dd4934f9c3caeba75bec3c4ea23e6ad42cd4
Veeresh5027 <veereshpattar5027@gmail.com>
2025-06-20 20:25:26 +0530

    quiz and quesion API added

---
+++ examserver/src/main/java/com/exam/controller/QuizController.java
@@ -0,0 +1,47 @@
+package com.exam.controller;
+
+
+import com.exam.Service.QuizService;
+import com.exam.model.exam.Quiz;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/quiz")
+public class QuizController {
+
+    @Autowired
+    private QuizService quizService;
+
+
+    @PostMapping("/")
+    public ResponseEntity<Quiz> addQuiz(@RequestBody Quiz quiz) {
+        return ResponseEntity.ok(this.quizService.addQuiz(quiz));
+    }
+
+    //update quiz
+    @PutMapping("/")
+    public ResponseEntity<Quiz> updateQuiz(@RequestBody Quiz quiz) {
+        return ResponseEntity.ok(this.quizService.updateQuiz(quiz));
+    }
+
+    //get all quizzes
+    @GetMapping("/")
+    public ResponseEntity<?> getQuizzes() {
+        return ResponseEntity.ok(this.quizService.getQuizzes());
+    }
+
+    //get single quiz
+    @GetMapping("/{qid}")
+    public ResponseEntity<Quiz> getQuiz(@PathVariable("qid") Long qid) {
+        return ResponseEntity.ok(this.quizService.getQuiz(qid));
+    }
+
+    //delete quiz
+    @DeleteMapping("/{qid}")
+    public void deleteQuiz(@PathVariable("qid") Long qid) {
+        this.quizService.deleteQuiz(qid);
+    }
+}
